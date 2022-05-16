ecbb6e65c079f82c3e0012228bdc955ca7c7cdb3
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-05-16 10:06:58 +0000

    [Add]マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :public do
+    resources :customers, only: [:show]
+  
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
